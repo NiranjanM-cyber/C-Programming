e5af06a2d2531b26e19adacdba175afe09751455
niranjan <your-email@example.com>
2026-06-11 16:30:52 +0530

    SUM AND AVERAGE USING ARRAY

--- array_add_average.c
+++ array_add_average.c
@@ -4,7 +4,9 @@ int main()
 	int i;
 	int n;
 	int sum = 0;
+	int average;
 
+	printf("\n");
 	printf("ENTER THE ARRAY :");
 	scanf("%d",&n);
 
@@ -13,19 +15,25 @@ int main()
 	for(int i = 0 ; i < n ; i++)
 	{
 		printf("ENTER THE NUMBER :");
-		scanf("%d",&arr[i]);	
+		scanf("%d",&arr[i]);
 	}
 
 	printf("VALUES IN THE ARRAY :");
-	scanf("%d",&arr[i]);
+
+	for(int i = 0 ; i < n ; i++)
+	{
+		printf("%d ",arr[i]);
+	}
 
 	for(int i = 0 ; i < n ; i++)
 	{
 		sum += arr[i];
-		
+		average = sum / n;
 	}
 
-	printf("SUM =%d",sum);
-
+	printf("\nSUM =%d\n",sum);
+	printf("AVERAGE =%d",average);
+	
+	printf("\n");
 	return 0;
 }
