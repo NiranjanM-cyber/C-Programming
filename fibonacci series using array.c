520d2ef73222a71810820bfe2e98343fbec32f00
niranjan <your-email@example.com>
2026-06-23 16:56:17 +0530

    fibonacci series using array

---
+++ fibonacci series using array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+
+	printf("ENTER THE NUMBER OF FIBONACCI SERIES AMOUNT :");
+	scanf("%d",&n);
+
+	int arr[n];
+
+	arr[0] = 0;
+	arr[1] = 1;
+
+	for(int i = 2;i <= n;i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2]; 
+	}
+
+	for(int i = 0;i < n;i++)
+	{
+		printf("%d \n",arr[i]);
+	}
+
+	return 0;
+}
