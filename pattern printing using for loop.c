657261043f67e0f3d04ceefc5947598c50bb26e9
niranjan <your-email@example.com>
2026-06-24 10:38:33 +0530

    PATTERN PRINTING OF '*' INCREMENT TO DECREMENT USING FOR LOOP

---
+++ pattern printing using for loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+
+	printf("ENTER THE VALUE :");
+	scanf("%d",&n);
+
+	for(int i = 1;i <= n;i++)
+	{
+
+		for(int j = n;j >= i;j--)
+		{
+			printf( "* ");
+		}
+
+		printf("\n");
+	}	
+	
+	return 0;
+}
