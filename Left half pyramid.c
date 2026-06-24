39741b6302befe7dfc5217441266408c6d553a1b
niranjan <your-email@example.com>
2026-06-24 16:20:32 +0530

    LEFT HALF PYRAMID USING FOR LOOP JUST THE DIFF FROM DIAMOND IS SPACE FOR THE FIRST FOR LOOP

---
+++ Left half pyramid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("ENTER THE VALUE :");
+	scanf("%d",&n);
+
+	for(int i = n;i >= 0;i--)
+	{
+		for (int j = 0;j < i;j++)
+		{
+			printf("  ");
+		}
+		for(int k = 0;k <= n - i;k++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
